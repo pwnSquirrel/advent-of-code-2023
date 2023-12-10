a7ab1f8d02ec5825986bdc8fcde90b2a493581db
pwn_squirrel <>
2023-12-10 16:30:00 +0100

    add day10 part 2 (ugly and with borrowed code)

--- 2023/day10/haskell/day10.hs
+++ 2023/day10/haskell/day10.hs
@@ -1,6 +1,5 @@
-{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+{-# OPTIONS_GHC -Wno-incomplete-patterns #-}
 
-{-# HLINT ignore "Use bimap" #-}
 import Data.List
 
 findNext :: Char -> (Int, Int) -> (Int, Int)
@@ -20,19 +19,34 @@ findStart grid =
       Just x = elemIndex 'S' $ grid !! y
    in (x, y)
 
-walkLoop :: [[Char]] -> (Int, Int) -> (Int, Int) -> Int -> Int
-walkLoop grid pos prev count =
-  let c = grid !! snd pos !! fst pos
-      d = (fst prev - fst pos, snd prev - snd pos)
-      nextD = findNext c d
-      next = (fst pos + fst nextD, snd pos + snd nextD)
-   in if c == 'S' then count else walkLoop grid next pos $ count + 1
+walkLoop :: [[Char]] -> (Int, Int) -> (Int, Int) -> [(Int, Int)] -> [(Int, Int)]
+walkLoop grid (px, py) (lx, ly) loop =
+  let ch = grid !! py !! px
+      (nx, ny) = findNext ch (lx, ly)
+      next = (px + nx, py + ny)
+   in if ch == 'S'
+        then (px, py) : loop
+        else walkLoop grid next (negate nx, negate ny) $ (px, py) : loop
 
--- findStartDirection :: [[Char]] -> (Int, Int) -> (Int, Int)
--- findStartDirection grid pos =
+-- shoelace theorem gives complete area
+shoeLace :: [(Int, Int)] -> Int
+shoeLace [_] = 0
+shoeLace ((x1, y1) : (x2, y2) : xs) = (y1 + y2) * (x2 - x1) + shoeLace ((x2, y2) : xs)
+
+part1 grid =
+  let (startx, starty) = findStart grid
+   in -- TODO find starting direction
+      flip div 2 $ length $ walkLoop grid (startx, starty + 1) (0, -1) []
+
+part2 :: [String] -> Int
+part2 grid =
+  let (startx, starty) = findStart grid
+      loop = walkLoop grid (startx, starty + 1) (0, -1) []
+   in -- picks theorem gives inner points
+      (abs (shoeLace loop) - length loop + 2) `div` 2
 
 main = do
   input <- readFile "../input.txt"
   let grid = lines input
-  let count = walkLoop grid (93, 25) (93, 24) 1
-  print $ count `div` 2
+  print $ part1 grid
+  print $ part2 grid
